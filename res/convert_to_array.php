2cad52d7d470c144b916e024a884b22ac119cab9
Brian Hogg <brian@bhconsulting.ca>
2014-02-22 16:22:44 -0500

    Grabbing the sum instead of the raw values

--- res/convert_to_array.php
+++ res/convert_to_array.php
@@ -2,28 +2,17 @@
 
 $filename = $argv[1];
 $file = fopen($filename, 'r');
-$accel_x = $accel_y = $accel_z = $g_x = $g_y = $g_z = array();
+$sumA = $sumG = array();
 while (($data = fgetcsv($file)) !== FALSE) {
-    if (count($data) > 7) {
-        $accel_x[] = $data[2];
-        $accel_y[] = $data[3];
-        $accel_z[] = $data[4];
-        $g_x[] = $data[5];
-        $g_y[] = $data[6];
-        $g_z[] = $data[7];
+    var_dump($data);
+    if (count($data) >= 7) {
+        $sumA[] = $data[2] + $data[3] + $data[4];
+        $sumG[] = $data[5] + $data[6] + $data[7];
     }
 }
 fclose($file);
-array_shift($accel_x);
-array_shift($accel_y);
-array_shift($accel_z);
-array_shift($g_x);
-array_shift($g_y);
-array_shift($g_z);
-$output = 'ax: [' . implode(',', $accel_x) . "],\n";
-$output .= 'ay: [' . implode(',', $accel_y) . "],\n";
-$output .= 'az: [' . implode(',', $accel_z) . "],\n";
-$output .= 'gx: [' . implode(',', $g_x) . "],\n";
-$output .= 'gy: [' . implode(',', $g_y) . "],\n";
-$output .= 'gz: [' . implode(',', $g_z) . "],\n";
+array_shift($sumA);
+array_shift($sumG);
+$output = 'sumA: [' . implode(',', $sumA) . "],\n";
+$output .= 'sumG: [' . implode(',', $sumG) . "],\n";
 file_put_contents($filename . '.txt', $output);
