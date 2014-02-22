aeb55493fdb446ac150cd9083901c8ee8cc4312a
Garth Gutenberg <garth.gutenberg@gmail.com>
2014-02-22 14:27:46 -0500

    Local socket.io. jquery.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
-	<script src="http://graph.kiwiwearables.com/javascripts/socket.io.js"></script>
+	<script src="js/socket.io.js"></script>
+	<script src="js/jquery-2.1.0.min.js"></script>
 	<script src="js/index.js"></script>
 </head>
 <body>
