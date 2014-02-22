17c23d2a1255e0b45c7de782be0325ea027e63b5
Garth Gutenberg <garth.gutenberg@gmail.com>
2014-02-22 14:50:15 -0500

    fdsa

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 <head>
 	<script src="js/socket.io.js"></script>
 	<script src="js/jquery-2.1.0.min.js"></script>
+	<script src="js/dtw.js"></script>
+<!--	<script src="js/custom.js"></script>-->
 	<script src="js/index.js"></script>
 </head>
 <body>
-
+<div id="detect"></div>
 </body>
 </html>
