6dcaf757aaee53eb25ec5a2eba6de6cf00696f30
Brian Hogg <brian@bhconsulting.ca>
2014-02-22 21:13:53 -0500

    Stuff is happening

--- res/convert_to_array.php
+++ res/convert_to_array.php
@@ -2,17 +2,30 @@
 
 $filename = $argv[1];
 $file = fopen($filename, 'r');
-$sumA = $sumG = array();
+$ax = $ay = $az = $gx = $gy = $gz = array();
+
 while (($data = fgetcsv($file)) !== FALSE) {
     var_dump($data);
     if (count($data) >= 7) {
-        $sumA[] = $data[2] + $data[3] + $data[4];
-        $sumG[] = $data[5] + $data[6] + $data[7];
+        $ax[] = $data[2];
+        $ay[] = $data[3];
+        $az[] = $data[4];
+        $gx[] = $data[5];
+        $gy[] = $data[6];
+        $gz[] = $data[7];
     }
 }
 fclose($file);
-array_shift($sumA);
-array_shift($sumG);
-$output = 'sumA: [' . implode(',', $sumA) . "],\n";
-$output .= 'sumG: [' . implode(',', $sumG) . "],\n";
+array_shift($ax);
+array_shift($ay);
+array_shift($az);
+array_shift($gx);
+array_shift($gy);
+array_shift($gz);
+$output = 'ax: [' . implode(',', $ax) . "],\n";
+$output .= 'ay: [' . implode(',', $ay) . "],\n";
+$output .= 'az: [' . implode(',', $az) . "],\n";
+$output .= 'gx: [' . implode(',', $gx) . "],\n";
+$output .= 'gy: [' . implode(',', $gy) . "],\n";
+$output .= 'gz: [' . implode(',', $gz) . "],\n";
 file_put_contents($filename . '.txt', $output);
