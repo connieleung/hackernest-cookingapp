4219e3f0f15260bb00b523e7d8a3a731e9019063
Garth Gutenberg <garth.gutenberg@gmail.com>
2014-02-22 23:19:17 -0500

    Yellow pages modal

--- js/motion/api.js
+++ js/motion/api.js
@@ -1,21 +1,31 @@
 /*global $:false, jQuery:false, console:false */
 $( document ).ready( function() {
-	navigator.geolocation.getCurrentPosition( function( position ) {
-		console.log( position );
-		$.ajax( {
-			type: 'GET',
-			url: 'http://api.sandbox.yellowapi.com/FindBusiness/',
-			data: {
-				what: 'grocery',
-				where: 'cZ' + position.coords.longitude + ',' + position.coords.latitude,
-				UID: 'isis',
-				fmt: 'JSON',
-				lang: 'en',
-				apikey: '7584x2qs9a8pk938sbayt3wa'
-			},
-			success: function( data ) {
-				console.log( data );
-			}
+
+	$( '#missing-ingredients-button' ).on( 'click', function() {
+		navigator.geolocation.getCurrentPosition( function( position ) {
+			$( '#yellowpages-results' ).html( 'Retrieving grocery stores nearby...' );
+			$.ajax( {
+				type: 'GET',
+				url: 'http://api.sandbox.yellowapi.com/FindBusiness/',
+				data: {
+					what: 'grocery stores',
+					where: 'cZ' + position.coords.longitude + ',' + position.coords.latitude,
+					UID: 'isis',
+					fmt: 'JSON',
+					lang: 'en',
+					apikey: '7584x2qs9a8pk938sbayt3wa'
+				},
+				success: function( data ) {
+					var html = '';
+					data.listings.forEach( function( listing ) {
+						html += '<h3>' + listing.name + '</h3>';
+						html += '<p>' + listing.address.street + '<br>' + listing.address.city + '</p>';
+					} );
+					$( '#yellowpages-modal h2' ).html( 'Grocery stores near you:' );
+					$( '#yellowpages-results' ).html( html );
+				}
+			} );
 		} );
 	} );
+
 } );
